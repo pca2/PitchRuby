68aa2965d29dc3d8bbbdbbcb73d760bd9ba99531
Carleton Atwater <atwaterc@gmail.com>
2013-02-24 10:38:01 -0500

    Add script to obtain user creds. Requires file with dev creds.

---
+++ getusertoken.rb
@@ -0,0 +1,16 @@
+#quick script to obtain permenant Rdio token
+
+require './rdio'
+require './credentials_and_settings'
+# create an instance of the Rdio object with our consumer credentials
+rdio = Rdio.new([RDIO_CONSUMER_KEY, RDIO_CONSUMER_SECRET])
+
+# authenticate against the Rdio service
+url = rdio.begin_authentication('oob')
+puts 'Go to: ' + url
+print 'Then enter the code: '
+verifier = gets.strip
+rdio.complete_authentication(verifier)
+
+puts "RDIO_TOKEN " + rdio.token[0]
+puts "RDIO_TOKEN_SECRET " + rdio.token[1]
